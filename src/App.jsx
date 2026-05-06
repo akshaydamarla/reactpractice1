06def771bd2468369786157986d6d0c91213b4dd
akshaydamarla <akshaydamarla1@gmail.com>
2026-05-06 10:04:51 +0530

    learned Props and implemented them

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import Card from "./Card";
 import Footer from "./Footer";
 import Header from "./Header";
 import Phones from "./Phones";
+import iPhone13Pro from "./assets/iPhone13Pro.jpg"
+import iPhoneXR from "./assets/iPhoneXR.jpeg"
+import oppo from "./assets/oppoa5s.jpeg"
 
 function App(){
   return(
    <>
     <Header/>
-    <Card/>
+    <Card name="iPhone 13 Pro" img={iPhone13Pro} text="This is my latest iPhone"/>
+    <Card name="iPhone XR" img={iPhoneXR} text="This is my First iPhone"/>
+    <Card name="Oppo A5s" img={oppo} text="This is my First Android"/>
     <Phones/>
     <Footer/>
     </>
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,9 @@
-import iPhone13Pro from "./assets/iPhone13Pro.jpg"
-
-function Card(){
+function Card(props){
     return(
         <div className="card">
-            <img className ="card-img" src={iPhone13Pro} alt="iPhone 13 Pro"></img>
-            <h2 className="card-title">iPhone 13 Pro</h2>
-            <p className="card-text">This is My Mobile</p>
+            <img className ="card-img" src={props.img} alt={props.name}></img>
+            <h2 className="card-title">{props.name}</h2>
+            <p className="card-text">{props.text}</p>
         </div>
     );
 }
